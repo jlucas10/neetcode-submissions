a4fda461ce473632e326f57f120f105644f8c73e
Josiah Lucas <124224654+jlucas10@users.noreply.github.com>
2026-08-11 21:16:20 -0700

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        """
+        U: input is list of heights
+            output is result min height of container and length fo
+            container.
+            loop through list, return maximum area of container between 
+            lists 
+
+        P: set l and r
+            loop through 
+                area computation 
+                check max res, if output is more set res to max 
+
+            return result
+        """
+        
+        left = 0
+        right = len(heights) - 1
+        result = 0
+
+        while left < right:
+            # right - left is length
+            # min - is height of container, go by min due to water cant
+            # overfill 
+            area = min(heights[left], heights[right]) * (right - left)
+            result = max(result, area)
+            if heights[left] <= heights[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
